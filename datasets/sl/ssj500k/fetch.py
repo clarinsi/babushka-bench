bc275e777a35faec3e0988a60ad05b0581fc9aea
nljubesi <nljubesi@gmail.com>
2019-10-22 10:08:45 +0200

    ssj500k version corrected

--- datasets/sl/ssj500k/fetch.py
+++ datasets/sl/ssj500k/fetch.py
@@ -1,5 +1,5 @@
 import urllib2
-response=urllib2.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1181/ssj500k-en.TEI.zip')
+response=urllib2.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1210/ssj500k-en.TEI.zip')
 archive=response.read()
 file=open('ssj500k-en.TEI.zip','w')
 file.write(archive)
