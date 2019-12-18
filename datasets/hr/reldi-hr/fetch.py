88cf1a87396513c226f10bcd0a8b8d257711fd88
nljubesi <nljubesi@gmail.com>
2019-12-18 10:19:43 +0100

    reldi-* fetch modified to python3

--- datasets/hr/reldi-hr/fetch.py
+++ datasets/hr/reldi-hr/fetch.py
@@ -1,7 +1,7 @@
-import urllib2
-response=urllib2.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1241/ReLDI-hr.conllu.zip')
+import urllib.request
+response=urllib.request.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1241/ReLDI-hr.conllu.zip')
 archive=response.read()
-file=open('ReLDI-hr.conllu.zip','w')
+file=open('ReLDI-hr.conllu.zip','wb')
 file.write(archive)
 file.close()
 import zipfile
--- datasets/sr/reldi-sr/fetch.py
+++ datasets/sr/reldi-sr/fetch.py
@@ -1,7 +1,7 @@
-import urllib2
-response=urllib2.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1240/ReLDI-sr.conllu.zip')
+import urllib.request
+response=urllib.request.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1240/ReLDI-sr.conllu.zip')
 archive=response.read()
-file=open('ReLDI-sr.conllu.zip','w')
+file=open('ReLDI-sr.conllu.zip','wb')
 file.write(archive)
 file.close()
 import zipfile
