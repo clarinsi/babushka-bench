af98a359f795c2890dd6aeef30ff0db967d18717
Luka <krsnik.luka92@gmail.com>
2021-02-25 14:48:44 +0100

    Added raw versions of norm dataset.

--- datasets/hr/reldi-hr/prep.py
+++ datasets/hr/reldi-hr/prep.py
@@ -17,10 +17,13 @@ for line in open('ReLDI-hr.vert/reldi_hr.vert'):
       pointer=dev
     else:
       pointer=test
+  elif line.startswith('<g/>'):
+    previous_glue = True
   elif line.startswith('</s>'):
     pointer.append(s)
   elif line.startswith('<s '):
     s=[id_re.search(line).group(1)]
+    previous_glue = True
   elif line.startswith('<name'):
     ner_type='B-'+ner_re.search(line).group(1)
   elif line.startswith('</name>'):
@@ -28,23 +31,36 @@ for line in open('ReLDI-hr.vert/reldi_hr.vert'):
   else:
     if not any(line.startswith(t) for t in tags):
       line=line.split('\t')
-      s.append((line[0],line[1],line[3],line[4],line[5],ner_type))
+      if previous_glue:
+        glue = ''
+      else:
+        glue = ' '
+      previous_glue = False
+      s.append((line[0],line[1],line[3],line[4],line[5],ner_type, glue))
     if ner_type.startswith('B-'):
       ner_type='I-'+ner_type[2:]
 
-def write_list(lst,fname,norm=False):
+def write_list(lst,fname,norm=False,raw=False):
   f=open(fname,'w')
   for el in lst:
     sent_id=el[0]
     tokens=el[1:]
+    raw_list = []
     if not norm:
       f.write('# sent_id = '+sent_id+'\n')
     for idx,token in enumerate(tokens):
       if not norm:
         f.write(str(idx+1)+'\t'+token[0]+'\t'+token[1]+'\t'+token[3]+'\t'+token[2]+'\t'+token[4]+'\t_\t_\t_\t'+token[5]+'\n')
       else:
-        f.write(token[0]+'\t'+token[1]+'\n')
-    f.write('\n')
+        if not raw:
+          f.write(token[0]+'\t'+token[1]+'\n')
+        else:
+          raw_list.append(token[-1])
+          raw_list.append(token[0])
+    if not raw:
+      f.write('\n')
+    else:
+      f.write(''.join(raw_list) + '\n')
   f.close()
 write_list(train,'train_ner.conllu')
 write_list(dev,'dev_ner.conllu')
@@ -52,3 +68,6 @@ write_list(test,'test_ner.conllu')
 write_list(train,'train_norm.tbl',True)
 write_list(dev,'dev_norm.tbl',True)
 write_list(test,'test_norm.tbl',True)
+write_list(train,'train_norm.txt',True,True)
+write_list(dev,'dev_norm.txt',True,True)
+write_list(test,'test_norm.txt',True,True)
--- datasets/sl/janes-norm/prep.py
+++ datasets/sl/janes-norm/prep.py
@@ -13,23 +13,43 @@ for line in open('Janes-Norm.vert/janes.norm.vert'):
       pointer=dev
     else:
       pointer=test
+  elif line.startswith('<g/>'):
+    previous_glue = True
   elif line.startswith('</s>'):
     pointer.append(s)
   elif line.startswith('<s'):
     s=[]
+    previous_glue = True
   else:
     if not any(line.startswith(t) for t in tags):
       line=line.split('\t')
-      s.append((line[0],line[1]))
+      if previous_glue:
+        glue = ''
+      else:
+        glue = ' '
+      previous_glue = False
+      s.append([line[0],line[1],glue])
 
-def write_list(lst,fname):
+def write_list(lst,fname,raw=False):
   f=open(fname,'w')
   for el in lst:
     tokens=el
+    raw_list = []
     for idx,token in enumerate(tokens):
-      f.write(token[0]+'\t'+token[1]+'\n')
-    f.write('\n')
+      if not raw:
+        f.write(token[0]+'\t'+token[1]+'\n')
+      else:
+        if len(token) == 3:
+          raw_list.append(token[2])
+          raw_list.append(token[0])
+    if not raw:
+      f.write('\n')
+    else:
+      f.write(''.join(raw_list) + '\n')
   f.close()
 write_list(train,'train_norm.tbl')
 write_list(dev,'dev_norm.tbl')
 write_list(test,'test_norm.tbl')
+write_list(train,'train_norm.txt',True)
+write_list(dev,'dev_norm.txt',True)
+write_list(test,'test_norm.txt',True)
