f7add717c5ed8882026fb8c553ee264853357480
SuzanaB <suz.basic@gmail.com>
2021-10-10 16:27:14 +0200

    Update fetch.py

--- datasets/hr/hr500k/fetch.py
+++ datasets/hr/hr500k/fetch.py
@@ -1,8 +1,9 @@
 import urllib.request
+import zipfile
+
 response = urllib.request.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1183/hr500k.TEI.zip')
 archive = response.read()
 file = open('hr500k.TEI.zip', 'wb')
 file.write(archive)
 file.close()
-import zipfile
 zipfile.ZipFile('hr500k.TEI.zip').extractall('.')
--- datasets/sl/ssj500k/fetch.py
+++ datasets/sl/ssj500k/fetch.py
@@ -1,8 +1,9 @@
-import urllib2
-response=urllib2.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1210/ssj500k-en.TEI.zip')
-archive=response.read()
-file=open('ssj500k-en.TEI.zip','w')
+import urllib.request
+import zipfile
+
+response = urllib.request.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1210/ssj500k-en.TEI.zip')
+archive = response.read()
+file = open('ssj500k-en.TEI.zip','w')
 file.write(archive)
 file.close()
-import zipfile
 zipfile.ZipFile('ssj500k-en.TEI.zip').extractall('.')
