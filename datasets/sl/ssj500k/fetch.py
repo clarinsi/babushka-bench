4a9f4c2d05e90804552279dabc1b0ff8b05a7f9e
SuzanaB <suz.basic@gmail.com>
2021-10-10 16:28:36 +0200

    Update fetch.py

--- datasets/sl/ssj500k/fetch.py
+++ datasets/sl/ssj500k/fetch.py
@@ -3,7 +3,7 @@ import zipfile
 
 response = urllib.request.urlopen('https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1210/ssj500k-en.TEI.zip')
 archive = response.read()
-file = open('ssj500k-en.TEI.zip','w')
+file = open('ssj500k-en.TEI.zip', 'wb')
 file.write(archive)
 file.close()
 zipfile.ZipFile('ssj500k-en.TEI.zip').extractall('.')
