5eca0b959dc9cf59c67ba6f1a4d8815e89a87330
SuzanaB <suz.basic@gmail.com>
2021-10-18 01:04:39 +0200

    Fix non-normalized forms

--- datasets/sl/janes-tag/prep.py
+++ datasets/sl/janes-tag/prep.py
@@ -20,6 +20,7 @@ with open('Janes-Tag.conllu/Janes-Tag.conllu', 'r', encoding='utf-8') as f:
     current_xpos = []
     current_feats = []
     current_misc = []
+    current_form = ''
     for line in f:
         if line.startswith('# sent_id'):
             rand = random.random()
@@ -70,6 +71,7 @@ with open('Janes-Tag.conllu/Janes-Tag.conllu', 'r', encoding='utf-8') as f:
                     current_xpos = []
                     current_feats = []
                     current_misc = []
+                    current_form = ''
 
                     line = '\t'.join(
                         [str(token_counter), current_form, lemma, upos, xpos, feats, head, deprel, deps, misc, '\n'])
@@ -88,10 +90,12 @@ with open('Janes-Tag.conllu/Janes-Tag.conllu', 'r', encoding='utf-8') as f:
                 else:
                     misc = '_'
                 norm_line = '\t'.join([str(token_counter), norm, lemma, upos, xpos, feats, head, deprel, deps, misc, '\n'])
-                all_line = '\t'.join([str(token_counter), form, norm, lemma, upos, xpos, feats, '\n'])
+                all_line = '\t'.join([str(token_counter), current_form if current_form else form, norm, lemma, upos,
+                                      xpos, feats, '\n'])
             else:
                 norm_line = line
-                all_line = '\t'.join([str(token_counter), form, form, lemma, upos, xpos, feats, '\n'])
+                all_line = '\t'.join([str(token_counter), current_form if current_form else form,
+                                      current_form if current_form else form, lemma, upos, xpos, feats, '\n'])
 
         elif line == '\n':
             norm_line = line
